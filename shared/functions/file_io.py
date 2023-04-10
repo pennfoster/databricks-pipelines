772287e017bc25ff0011e5f248d3fe0deb410653
Noam Blanks <noam.blanks@carruslearn.com>
2023-04-10 12:50:31 -0400

    new section to allow for optional filetype specification

--- shared/functions/file_io.py
+++ shared/functions/file_io.py
@@ -1,14 +1,20 @@
+import logging
 from uuid import uuid4
+
 import pendulum
 
 
-def generate_unique_filename(base: str) -> str:
+def generate_unique_filename(base: str, filetype: str = None) -> str:
     if "/" in base:
         raise ValueError(
             "Forward slash in filename could result in unintended subdirctories"
         )
+    if filetype:
+        if not filetype.startswith("."):
+            logging.warning("filetype argument should start with a period")
+            filetype = f".{filetype}"
 
     ts = pendulum.now().int_timestamp
     uuid = uuid4().hex
 
-    return f"{base}_ts-{ts}_uuid-{uuid}"
+    return f"{base}_ts-{ts}_uuid-{uuid}{filetype or ''}"
