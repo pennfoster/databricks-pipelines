505b381d1435ed3afbdbf40abfffca98b9e6021d
Noam Blanks <noam.blanks@carruslearn.com>
2023-04-24 18:09:17 -0400

    dummy tasks for dummies"

---
+++ notebooks/dummy/dummy_1.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+
+print("this is a dummy task for dummies")
+
+# COMMAND ----------
+
+dbutils.notebook.exit("Hello, World!")
---
+++ notebooks/dummy/dummy_3.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+
+print("this is a dummy task for dummies")
+
+# COMMAND ----------
+
+dbutils.notebook.exit("I'm definitely not sentient!")
