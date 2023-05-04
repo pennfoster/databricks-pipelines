0cbde4082f8c9db37106ffb1847382c5d968b840
Noam Blanks <noam.blanks@carruslearn.com>
2023-05-04 17:01:59 -0400

    better failures and a revised file pattern

--- notebooks/monterey/get_transaction_files.py
+++ notebooks/monterey/get_transaction_files.py
@@ -14,6 +14,8 @@ sftp_vars = {
     "password": dbutils.secrets.get(get_key_vault_scope(), "monterey-sftp-pw"),
     "hostkeys": None,
 }
+failures = {}
+
 # COMMAND ----------
 with SFTPBase(**sftp_vars, data_source="monterey") as sftp:
     host_directory = "./Transactions"
@@ -23,11 +25,14 @@ with SFTPBase(**sftp_vars, data_source="monterey") as sftp:
 
     remote_file_list = sftp.connection.listdir(host_directory)
 
-    pattern = r"^(?P<table>Transactions|Declines)(?P<date>\d{8})\.csv$"
+    pattern = r"^(?P<table>Transactions|Declines)_?(?P<date>\d{4}-?\d{2}-?\d{2})_?(?P<time>\d{2}-?\d{2}-?\d{2})?\.csv$"
     for remote_file in remote_file_list:
         m = re.match(pattern, remote_file)
         if not m:
             logging.warning("file name found not as expected: %s" % remote_file)
+            failures[
+                "sftp_name_pattern"
+            ] = "One or more remote files do not conform to expected name pattern"
             continue
 
         table = m["table"]
@@ -52,3 +57,9 @@ with SFTPBase(**sftp_vars, data_source="monterey") as sftp:
             host_file=remote_file,
             local_subdir=table,
         )
+
+# COMMAND ----------
+if any(failures):
+    import json
+
+    raise Exception(json.dumps(failures))
