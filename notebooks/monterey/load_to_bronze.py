90dd8ce93097c660e1fa0c6b64cc93d66da3835e
Noam Blanks <noam.blanks@carruslearn.com>
2023-05-05 19:12:34 -0400

    finally finished the bronze load. one more test

--- notebooks/monterey/load_to_bronze.py
+++ notebooks/monterey/load_to_bronze.py
@@ -45,38 +45,35 @@ for table_name, columns in table_variables.items():
             raw_df = add_insert_data(spark.read.csv(path=file, header=True))
             continue
         sub_df = add_insert_data(spark.read.csv(path=file, header=True))
-        raw_df.unionByName(sub_df, allowMissingColumns=True)
+        raw_df = raw_df.unionByName(sub_df, allowMissingColumns=True)
 
     # Union existing with new
-    dirty_bronze_df = spark.read.load(bronze_dest_path, "delta").unionByName(
+    dirty_df = spark.read.load(bronze_dest_path, "delta").unionByName(
         raw_df, allowMissingColumns=True
     )
 
-    # ? df_w_insert_data.write.format("delta").mode("append").option(
-    # ?     "mergeSchema", True
-    # ? ).option("overwriteSchema", True,).save(bronze_dest_path)
-
     versioned_df = add_data_version_flags(
-        df=dirty_bronze_df,
+        df=dirty_df,
         internal_date_col=columns["intertnal_date_column"],
         metadata_date_col="_bronze_insert_ts",
     )
 
     # remove unecessary duplicates
-    cleaned_bronze_df = dirty_bronze_df.filter(
-        (dirty_bronze_df["_initial_data_for_date"] == True)
-        | (dirty_bronze_df["_most_recent_data_for_date"] == True)
-        | (dirty_bronze_df["_deleted_at_source"] == True)
+    clean_df = versioned_df.filter(
+        (versioned_df["_initial_data_for_date"] == True)
+        | (versioned_df["_most_recent_data_for_date"] == True)
+        | (versioned_df["_deleted_at_source"] == True)
     )
 
     #! Is overwriting here correct? Should table be edited in place instead?
-    cleaned_bronze_df.write.format("delta").mode("overwrite").option(
-        "mergeSchema", True
-    ).option("overwriteSchema", True,).save(bronze_dest_path)
+    clean_df.write.format("delta").mode("overwrite").option("mergeSchema", True).option(
+        "overwriteSchema",
+        True,
+    ).save(bronze_dest_path)
 
-    # processed_path = f"{raw_source_path}/processed"
-    # dbutils.fs.mkdirs(processed_path)
-    # for file in csv_files:
-    #     dbutils.fs.mv(file, processed_path)
+    processed_path = f"{raw_source_path}/processed"
+    dbutils.fs.mkdirs(processed_path)
+    for file in csv_files:
+        dbutils.fs.mv(file, processed_path)
 
     print(f"{table_name} complete")
