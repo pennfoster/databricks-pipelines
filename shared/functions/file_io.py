9782867cd0afbef83152644a9ac15482583fb3ce
Noam Blanks <noam.blanks@carruslearn.com>
2023-05-08 11:40:33 -0400

    filename timestamp formatting

--- shared/functions/file_io.py
+++ shared/functions/file_io.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 import pendulum
 
 
-def generate_unique_filename(base: str, filetype: str = "") -> str:
+def generate_unique_filename(base: str, filetype: str = None) -> str:
     if "/" in base:
         raise ValueError(
             "Forward slash in filename could result in unintended subdirctories"
@@ -13,7 +13,7 @@ def generate_unique_filename(base: str, filetype: str = "") -> str:
         logging.warning("filetype argument should start with a period")
         filetype = f".{filetype}"
 
-    ts = pendulum.now(tz="utc").format("Y-MM-DDTHH:mm:ssZZ")
+    ts = pendulum.now(tz="utc").format("YMMDDTHHmmss-zz")
     uuid = str(uuid4().hex)
 
-    return f"{base}_{ts}_{uuid}{filetype}"
+    return f"{base}_{ts}_{uuid}{filetype or ''}"
