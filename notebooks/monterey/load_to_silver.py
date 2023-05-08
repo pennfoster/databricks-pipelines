eb40d888757128e06c305c34aa75a224370a613a
Noam Blanks <noam.blanks@carruslearn.com>
2023-05-08 10:17:07 -0400

    silver table notebook

--- notebooks/monterey/load_to_silver.py
+++ notebooks/monterey/load_to_silver.py
@@ -1,52 +1,20 @@
 # Databricks notebook source
 from shared.functions.azure_utilities import get_mount_paths
 
-# COMMAND ----------
-
-mnt = get_mount_paths("monterey")
-
-for table_name in mnt.bronze:
-    bronze_path = f"{mnt.bronze}/{table_name}"
-    silver_path = f"{mnt.silver}/{table_name}"
-
-    bronze_table = spark.read.load(path=bronze_path, format="delta")
-# COMMAND ----------
-#   select distinct
-#      *
-#   from bronze
-#   excluding fp, branch
-#   where ts = max(ts) over (partition by non-metadata)
-#
-
-
-# display(df.distinct().orderBy(["ClassAccount", "ContractID", "PaymentEffectiveDate"]))
-
+DATA_SOURCE = "monterey"
+bronze_path = get_mount_paths(DATA_SOURCE).bronze
+silver_path = get_mount_paths(DATA_SOURCE).silver
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC
-# MAGIC SELECT
-# MAGIC   contractid,
-# MAGIC   classaccount,
-# MAGIC   acctnumber,
-# MAGIC   paymentEffectivedate,
-# MAGIC   count(*) as c
-# MAGIC FROM
-# MAGIC   (select distinct *
-# MAGIC   from delta.`/mnt/sadatalakehousedev001_bronze/monterey/Declines`)
-# MAGIC group by 1,2,3,4
-# MAGIC having c >1
-# MAGIC ;
+for table_fs in dbutils.fs.ls(bronze_path):
+    bronze_df = spark.read.load(path=table_fs.path, format="delta")
 
-# COMMAND ----------
+    current_data = bronze_df.filter(
+        (bronze_df["_most_recent_data_for_date"] == True)
+        & (bronze_df["_deleted_at_source"] == False)
+    )
 
-# MAGIC
-# MAGIC %sql
-# MAGIC select distinct
-# MAGIC   *
-# MAGIC FROM delta.`/mnt/sadatalakehousedev001_bronze/monterey/Declines`
-# MAGIC where contractid=0933160
-# MAGIC and classaccount=503087010
-# MAGIC and acctnumber =8767
-# MAGIC and paymenteffectivedate='2020-05-01'
+    current_data.write.format("delta").mode("overwrite").option(
+        "mergeSchema", True
+    ).option("overwriteSchema", True,).save(f"{silver_path}/{table_fs.name}")
